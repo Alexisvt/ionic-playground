61dafee1586714f50a7b2334b9cfa0c890935bd7
Alexis Villegas <alexis.villegas@eimprovement.com>
2020-03-27 16:27:43 -0500

    feat(ActionSheetPage): add ActionSheet component

--- 03-components/src/app/pages/home/home.page.ts
+++ 03-components/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+import { IComponent } from './component.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  components: IComponent[] = [
+    {
+      icon: 'american-football',
+      name: 'Action sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert',
+    },
+  ];
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit() {}
 }
