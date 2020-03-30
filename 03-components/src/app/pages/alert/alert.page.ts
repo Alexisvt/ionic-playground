14f47b391e5406aba6ed72b3bbe19858b88921b7
Alexis Villegas <alexis.villegas@eimprovement.com>
2020-03-30 08:12:45 -0500

    feat(Alert): add alert component
    Add an alert component to allow change the title dynamically.

--- 03-components/src/app/pages/alert/alert.page.ts
+++ 03-components/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+  constructor(public alertController: AlertController) {}
 
-  constructor() { }
+  title = 'Alert';
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: blah => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
+  async presentSecondAlert() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'Type new title',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: blah => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Okay',
+          handler: ({ title }) => {
+            this.title = title;
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
